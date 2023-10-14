fb29148edc32969544d359fcad74799b26b690af
songheng13 <hsong@smith.edu>
2023-10-13 23:05:29 -0400

    set up project1

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 from os import environ
 
 SESSION_CONFIGS = [
-    # dict(
-    #     name='public_goods',
-    #     app_sequence=['public_goods'],
-    #     num_demo_participants=3,
-    # ),
+    dict(
+         name='Sharing_Decision',
+         app_sequence=['Project1'],
+         num_demo_participants=3,
+     ),
 ]
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
